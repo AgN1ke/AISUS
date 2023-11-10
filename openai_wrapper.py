dec7fb402a0dc254b9a74ac6856fe482109f2cf6
Sky <IzalithSky@users.noreply.github.com>
2023-11-10 22:01:00 +0000

    OpenAIWrapper fix

--- openai_wrapper.py
+++ openai_wrapper.py
@@ -1,14 +1,14 @@
 # openai_wrapper.py
-import openai
+from openai import OpenAI
 
 
 class OpenAIWrapper:
     def __init__(self, api_key: str):
-        openai.api_key = api_key
+        self.api_key = api_key
 
-    @staticmethod
-    def chat_completion(model: str, messages: list, max_tokens: int):
-        return openai.ChatCompletion.create(
+    def chat_completion(self, model: str, messages: list, max_tokens: int):
+        client = OpenAI(api_key=self.api_key)
+        return client.chat.completions.create(
             model=model,
             messages=messages,
             max_tokens=max_tokens)
