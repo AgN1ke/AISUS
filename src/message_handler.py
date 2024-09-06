9c258990ea36e887be888dbc71499e0d0e074c00
AgNike <nikita.zagamula@gmail.com>
2024-09-06 16:01:29 +0300

    add password fix

--- src/message_handler.py
+++ src/message_handler.py
@@ -19,6 +19,11 @@ class CustomMessageHandler:
         chat_id = update.effective_chat.id
         message_text = update.message.text
 
+        # Перевірка, чи бот має бути активований в публічному чаті (тільки через тег або відповідь)
+        if not await self._should_process_message(context.bot, MessageWrapper(update)):
+            print("Message not processed due to filter.")
+            return
+
         # Check if the user is already authenticated
         if not self.authenticated_users.get(chat_id):
             if message_text == self.config.get_system_messages().get('password'):
@@ -37,12 +42,18 @@ class CustomMessageHandler:
         except Exception as e:
             print(f"Error handling message: {e}")
 
+    async def _should_process_message(self, bot, message):
+        """Determine if the message should be processed."""
+        bot_username = (await bot.get_me()).username
+        # Only process the message if it's a private chat or the bot is mentioned/replied to
+        return (
+            message.chat_type == 'private' or
+            (message.text and f"@{bot_username}" in message.text) or
+            (message.reply_to_message and message.reply_to_message_from_user_username == bot_username)
+        )
+
     async def _handle_message(self, bot, message: MessageWrapper):
         """Handle incoming messages and generate responses."""
-        if not await self._should_process_message(bot, message):
-            print("Message not processed due to filter.")
-            return
-
         user_message, is_voice = await self._process_message_content(message)
         if not user_message:
             print("No user message found.")
@@ -63,16 +74,6 @@ class CustomMessageHandler:
             await message.reply_text("Вибачте, але я не можу продовжити цю розмову.")
 
         self.chat_history_manager.prune_history(chat_id, 124000)
-
-    async def _should_process_message(self, bot, message):
-        """Determine if the message should be processed."""
-        bot_username = (await bot.get_me()).username
-        return (
-                message.chat_type == 'private' or
-                (message.text and f"@{bot_username}" in message.text) or
-                (message.reply_to_message and message.reply_to_message_from_user_username == bot_username) or
-                message.voice
-        )
 
     async def _process_message_content(self, message):
         """Process the content of the message, whether it's voice or text."""
